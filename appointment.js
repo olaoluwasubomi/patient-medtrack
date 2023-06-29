f0a8245d198b0302deb5c0f825c12ae8ecd36fe2
olaoluwasubomi <73838337+olaoluwasubomi@users.noreply.github.com>
2023-06-29 15:06:41 +0100

    Add files via upload

--- appointment.js
+++ appointment.js
@@ -6,3 +6,8 @@ function myFunction1(){
     let two = document.getElementById("reschedule");
     two.onclick = window.alert("Kindly book a new appointment to reschedule");
 }
+
+function book(){
+    let pick = document.getElementById("bookanappointment");
+    pick.onclick = window.alert("Your appointed has been submitted for approval");
+}
